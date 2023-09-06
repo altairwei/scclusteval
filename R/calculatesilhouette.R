6b714877bba3e8844a492dfe889c6931fdd47a5c
Altair Wei <altair_wei@outlook.com>
2023-09-06 17:03:52 +0800

    add reduction param to CalculateSilhouette

--- R/calculatesilhouette.R
+++ R/calculatesilhouette.R
@@ -3,6 +3,7 @@
 #' This is calculated from Seurat object
 #' @param object A Seurat object with Idents set to cluster ids (factors)
 #' @param dims default 1:50  dimension to use in the PCA space to calculate
+#' @param reduction Name of reduction to calculate Silhouette on.
 #' eucledian distance
 #'
 #' @return a dataframe with silhouette width for each cell. see also \code{\link[cluster]{silhouette}}
@@ -11,17 +12,17 @@
 #' @examples
 #' CalculateSilhouette(pbmc_small, dims = 1:15)
 #'
-CalculateSilhouette<- function(object, dims = 1:50){
-        if (length(dims) > ncol(object@reductions$pca@cell.embeddings)) {
-                stop("please specify PCA dims smaller than calculated")
-        }
-        cell_distance<- dist(object@reductions$pca@cell.embeddings[, dims])
-        # or as.integer
-        cell_cluster<- as.numeric(as.character(Seurat::Idents(object)))
-        silhouette_score<- cluster::silhouette(cell_cluster, cell_distance)
-        silhouette_score<- tibble::tibble(cluster = silhouette_score[,1],
-                                          width = silhouette_score[,3],
-                                          cell = colnames(object)) %>%
-                dplyr::mutate(cluster = as.factor(cluster))
-        return(silhouette_score)
+CalculateSilhouette <- function(object, dims = 1:50, reduction = "pca"){
+  embed <- Seurat::Embeddings(object, reduction = reduction)
+  if (length(dims) > ncol(embed)) {
+    stop("please specify PCA dims smaller than calculated")
+  }
+
+  cell_distance <- dist(embed[, dims])
+  cell_cluster <- Seurat::Idents(object)
+  silhouette_score <- cluster::silhouette(as.integer(cell_cluster), cell_distance)
+  silhouette_score <- tibble::tibble(cluster = cell_cluster,
+                                     width = silhouette_score[, 3],
+                                     cell = names(cell_cluster))
+  return(silhouette_score)
 }
